82c0642f31d8b53657d4098f69f0e19edbb24488
minerisslender <flynnjt@icloud.com>
2018-12-09 02:00:01 +0000

    Fixed weapons

--- virus/entities/weapons/weapon_ov_flak.lua
+++ virus/entities/weapons/weapon_ov_flak.lua
@@ -1,100 +1,88 @@
--- Flak Gun
-
+	
 SWEP.PrintName = "#weapon_ov_flak"
-SWEP.UseHands = true
-
-SWEP.ViewModelFOV = 58
-SWEP.ViewModelFlip = false
-SWEP.ViewModel = "models/weapons/c_357.mdl"
-SWEP.WorldModel = "models/weapons/w_357.mdl"
-
-SWEP.Primary.ClipSize = 1
-SWEP.Primary.DefaultClip = 1
-SWEP.Primary.Automatic = false
-SWEP.Primary.Ammo = "Flak"
-
-SWEP.Secondary.ClipSize = -1
-SWEP.Secondary.DefaultClip = -1
-SWEP.Secondary.Automatic = false
-SWEP.Secondary.Ammo = "none"
-
-SWEP.Weight = 2
-SWEP.AutoSwitchTo = false
-SWEP.AutoSwitchFrom = false
-
-SWEP.Slot = 1
-SWEP.SlotPos = 1
-SWEP.DrawAmmo = true
-SWEP.DrawCrosshair = true
-SWEP.BounceWeaponIcon = false
-SWEP.DrawWeaponInfoBox = false
-
-local WeaponSound = Sound( "weapons/flaregun/fire.wav" )
+SWEP.Category				= "GMod Tower Tribute"
+SWEP.Author				= "Babel Industries"
+SWEP.Contact				= ""
+SWEP.Purpose				= ""
+SWEP.Instructions				= "Fires an impossibly powerful burst of flak. Watch out for the spread, and the low ammo count."
+SWEP.MuzzleAttachment			= "1" 	-- Should be "1" for CSS models or "muzzle" for hl2 models
+SWEP.ShellEjectAttachment			= "2" 	-- Should be "2" for CSS models or "1" for hl2 models
+SWEP.PrintName				= "Flak Handgun"		-- Weapon name (Shown on HUD)	
+SWEP.Slot				= 1				-- Slot in the weapon selection menu
+SWEP.SlotPos				= 21			-- Position in the slot
+SWEP.DrawAmmo				= true		-- Should draw the default HL2 ammo counter
+SWEP.DrawWeaponInfoBox			= true		-- Should draw the weapon info box
+SWEP.BounceWeaponIcon   		= 	false	-- Should the weapon icon bounce?
+SWEP.DrawCrosshair			= true		-- set false if you want no crosshair
+SWEP.Weight				= 30			-- rank relative ot other weapons. bigger is better
+SWEP.AutoSwitchTo			= true		-- Auto switch to if we pick it up
+SWEP.AutoSwitchFrom			= true		-- Auto switch from if you pick up a better weapon
+SWEP.HoldType 				= "pistol"		-- how others view you carrying the weapon
+-- normal melee melee2 fist knife smg ar2 pistol rpg physgun grenade shotgun crossbow slam passive 
+-- you're mostly going to use ar2, smg, shotgun or pistol. rpg and crossbow make for good sniper rifles
 
 
--- Initialize the weapon
-function SWEP:Initialize()
+SWEP.SelectiveFire		= false
+SWEP.CanBeSilenced		= false
+SWEP.ViewModelFOV			= 70
+SWEP.ViewModelFlip			= true
+SWEP.ViewModel				= "models/weapons/v_vir_flakhg.mdl"	-- Weapon view model
+SWEP.WorldModel				= "models/weapons/w_vir_flakhg.mdl"	-- Weapon world model
+SWEP.Base				= "tfa_gun_base"
+SWEP.Spawnable				= true
+SWEP.AdminSpawnable			= true
+SWEP.FiresUnderwater = false
+SWEP.Akimbo = false
 
-    self:SetHoldType( "revolver" )
+SWEP.DisableChambering = true
 
-end
+SWEP.Primary.Sound			= Sound("Weapon_Flak.Fire")		-- Script that calls the primary fire sound
+SWEP.Primary.RPM			= 90			-- This is in Rounds Per Minute
+SWEP.Primary.ClipSize			= 1	-- Size of a clip
+SWEP.Primary.DefaultClip		= 120		-- Bullets you start with
+SWEP.Primary.KickUp			= 1.70					-- This is the maximum upwards recoil (rise)
+SWEP.Primary.KickDown			= 1.70							-- This is the maximum downwards recoil (skeet)
+SWEP.Primary.KickHorizontal			= 1.70							-- This is the maximum sideways recoil (no real term)
+SWEP.Primary.StaticRecoilFactor = 0.45 	--Amount of recoil to directly apply to EyeAngles.  Enter what fraction or percentage (in decimal form) you want.  This is also affected by a convar that defaults to 0.5.
+SWEP.MaxPenetrationCounter= 5
+SWEP.Primary.Automatic			= false		-- Automatic = true; Semi Auto = false
+SWEP.Primary.Ammo			= "357"			-- pistol, 357, smg1, ar2, buckshot, slam, SniperPenetratedRound, AirboatGun
+-- Pistol, buckshot, and slam always ricochet. Use AirboatGun for a light metal peircing shotgun pellets
 
+SWEP.Secondary.IronFOV			= 55		-- How much you 'zoom' in. Less is more! 	
 
--- Primary attack
-function SWEP:PrimaryAttack()
+SWEP.IronSightsSensitivity = 0.75
 
-	if ( !self:CanPrimaryAttack() ) then return end
+SWEP.data 				= {}				--The starting firemode
+SWEP.data.ironsights			= 1
 
-	self.Weapon:EmitSound( WeaponSound, 90, 90 )
-	
-	self:ShootBullet( 50, 6, 0.06 )
-	
-	self:TakePrimaryAmmo( 1 )
-	
-	self.Owner:ViewPunch( Angle( -12, 0, 0 ) )
+SWEP.Primary.SpreadMultiplierMax = 1.5 --How far the spread can expand when you shoot.
+SWEP.Primary.SpreadIncrement = 1/3.5 --What percentage of the modifier is added on, per shot.
+SWEP.Primary.SpreadRecovery = 3 --How much the spread recovers, per second.
 
-	self:SetNextPrimaryFire( CurTime() + 1 )
+SWEP.Primary.NumShots	= 8		-- How many bullets to shoot per trigger pull
+SWEP.Primary.Damage		= 50	-- Base damage per bullet
+SWEP.Primary.Spread		= .07	-- Define from-the-hip accuracy 1 is terrible, .0001 is exact)
+SWEP.Primary.IronAccuracy = .07 -- Ironsight accuracy, should be the same for shotguns
 
-end
+-- Enter iron sight info and bone mod info below
+SWEP.SightsPos = Vector(5.639, 0, 2.16)
+SWEP.SightsAng = Vector(-0.142, -0.139, 0)
+SWEP.RunSightsPos = Vector ()
+SWEP.RunSightsAng = Vector ()
+SWEP.InspectPos = Vector(-5.611, -14.639, 4.12)
+SWEP.InspectAng = Vector(15.539, -57.735, -10)
 
-
--- Secondary attack
-function SWEP:SecondaryAttack()
-
-    return
-
-end
-
-
--- Shoot bullets
-function SWEP:ShootBullet( damage, num_bullets, aimcone )
-
-	local bullet = {}
-	bullet.Num 		= num_bullets
-	bullet.Src 		= self.Owner:GetShootPos()
-	bullet.Dir 		= self.Owner:GetAimVector()
-	bullet.Spread 	= Vector( aimcone, aimcone, 0 )
-	bullet.Tracer	= 1
-	bullet.TracerName = "flaktracer"
-	bullet.Force	= 50
-	bullet.Damage	= damage
-	bullet.AmmoType = "Flak"
-
-	self.Owner:FireBullets( bullet )
-
-	self:ShootEffects()
-
-end
-
-
-if ( CLIENT ) then
-
-	-- Draw the weapon selection box
-	function SWEP:DrawWeaponSelection( x, y, w, h, a )
-	
-		draw.RoundedBox( 6, x, y, w, h, Color( 0, 0, 100, a - 100 ) )
-		draw.SimpleText( ".", "HL2MPTypeDeath", x + ( w / 2 ), y + ( h / 2 ), Color( 255, 255, 255, a ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
-	
-	end
-
-end
+SWEP.Offset = {
+        Pos = {
+        Up = -2,
+        Right = 1.6,
+        Forward = 6.1,
+        },
+        Ang = {
+        Up = -3.6,
+        Right = -4,
+        Forward = 180,
+        },
+		Scale = 1
+}
